f869ee5d7ce88006e749dfa78c1ea021199dbd30
miracle <panchireddiindumathi@gmail.com>
2024-07-30 11:16:03 +0530

    s commit

--- Capitalize.java
+++ Capitalize.java
@@ -1,22 +1,22 @@
-package practice;
-import java.util.*;
-public class Capitalize {
-public static void main(String[] args)
-{
-Scanner sc=new Scanner(System.in);
+// package practice;
+// import java.util.*;
+// public class Capitalize {
+// public static void main(String[] args)
+// {
+// Scanner sc=new Scanner(System.in);
 
-System.out.println("enter a string");
-String s=sc.nextLine();
+// System.out.println("enter a string");
+// String s=sc.nextLine();
 
-String a[]=s.split(" ");
+// String a[]=s.split(" ");
  
-for(String b: a)
-{
-String s2=b.substring(0,1).toUpperCase();
-String s3=b.substring(1);
-String str=s2+s3;
-System.out.print(str+" ");
-}
+// // for(String b: a)
+// // {
+// // String s2=b.substring(0,1).toUpperCase();
+// // String s3=b.substring(1);
+// // String str=s2+s3;
+// // System.out.print(str+" ");
+// // }
  
-}
-}
+// }
+// }
